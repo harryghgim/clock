88471b92b3a4886d7aa7a627bf83dd638073dfd5
gwanghyeongim <harryghgim@gmail.com>
2022-05-14 23:28:55 +0900

    formatting

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ const Button = (props) => {
     <button id={id} type="button" onClick={onClick}>
       <span className="material-symbols-outlined">{name}</span>
     </button>
-  )
-}
+  );
+};
 
-const Session = ({ state, setState }) => {  
+const Session = ({ state, setState }) => {
   const { session, timer, play } = state;
-  
+
   function handleSessionUp() {
     if (play) {
       return;
     }
-    setState(state => ({
-      ...state, 
+    setState((state) => ({
+      ...state,
       session: session == 60 ? session : session + 1,
       timer: timer == 60 * 60 ? timer : timer + 60,
     }));
@@ -27,8 +27,8 @@ const Session = ({ state, setState }) => {
     if (play) {
       return;
     }
-    setState(state => ({
-      ...state, 
+    setState((state) => ({
+      ...state,
       session: session == 1 ? session : session - 1,
       timer: timer == 60 ? timer : timer - 60,
     }));
@@ -38,9 +38,17 @@ const Session = ({ state, setState }) => {
     <div>
       <h2 id="session-label">Session Length</h2>
       <div>
-        <Button id="session-decrement" name="arrow_downward" onClick={handleSessionDown}/>
+        <Button
+          id="session-decrement"
+          name="arrow_downward"
+          onClick={handleSessionDown}
+        />
         <span id="session-length">{session}</span>
-        <Button id="session-increment" name="arrow_upward" onClick={handleSessionUp}/>
+        <Button
+          id="session-increment"
+          name="arrow_upward"
+          onClick={handleSessionUp}
+        />
       </div>
     </div>
   );
@@ -52,8 +60,8 @@ const Break = ({ state, setState }) => {
     if (play) {
       return;
     }
-    setState(state => ({
-      ...state, 
+    setState((state) => ({
+      ...state,
       break: breakLength == 60 ? breakLength : breakLength + 1,
     }));
   }
@@ -62,8 +70,8 @@ const Break = ({ state, setState }) => {
     if (play) {
       return;
     }
-    setState(state => ({
-      ...state, 
+    setState((state) => ({
+      ...state,
       break: breakLength == 1 ? breakLength : breakLength - 1,
     }));
   }
@@ -71,60 +79,72 @@ const Break = ({ state, setState }) => {
     <div>
       <h2 id="break-label">Break Length</h2>
       <div>
-        <Button id="break-decrement" name="arrow_downward" onClick={handleBreakDown}/>
+        <Button
+          id="break-decrement"
+          name="arrow_downward"
+          onClick={handleBreakDown}
+        />
         <span id="break-length">{breakLength}</span>
-        <Button id="break-increment" name="arrow_upward" onClick={handleBreakUp}/>
+        <Button
+          id="break-increment"
+          name="arrow_upward"
+          onClick={handleBreakUp}
+        />
       </div>
     </div>
   );
 };
 
-const Timer = ({state, setState}) => {
+const Timer = ({ state, setState }) => {
   const { session, play, timer, break: breakLength, sessionFlag } = state;
-  const min = String(Math.floor(timer / 60)).padStart(2, '0');
-  const sec = String(timer % 60).padStart(2, '0');
+  const min = String(Math.floor(timer / 60)).padStart(2, "0");
+  const sec = String(timer % 60).padStart(2, "0");
 
   useEffect(() => {
     if (play) {
       const timeoutId = setTimeout(() => {
         if (timer === 0) {
-          setState(state => ({...state, sessionFlag: !sessionFlag}));
+          setState((state) => ({ ...state, sessionFlag: !sessionFlag }));
         } else {
-          setState(state => ({...state, timer: timer - 1}));
+          setState((state) => ({ ...state, timer: timer - 1 }));
         }
       }, 1000);
       return () => clearTimeout(timeoutId);
-    } 
+    }
   }, [timer, play]);
 
   useEffect(() => {
     if (timer === 0) {
-      setState(state => ({...state, timer: sessionFlag ? session * 60 : breakLength * 60}));
+      setState((state) => ({
+        ...state,
+        timer: sessionFlag ? session * 60 : breakLength * 60,
+      }));
     }
   }, [sessionFlag]);
 
   function reset() {
-    setState(state => ({
-      ...state, 
-      break: 5, 
-      session: 25, 
-      timer: 25 * 60, 
+    setState((state) => ({
+      ...state,
+      break: 5,
+      session: 25,
+      timer: 25 * 60,
       play: false,
       sessionFlag: true,
     }));
-    
   }
   function handleStartStop() {
-    setState(state => ({...state, play: !play}));
+    setState((state) => ({ ...state, play: !play }));
     console.log(!play ? "played" : "paused");
   }
   return (
     <div>
       <h2 id="timer-label">{sessionFlag ? "Session" : "Break"}</h2>
-      <div id="time-left">{min}:{sec}</div>
+      <div id="time-left">
+        {min}:{sec}
+      </div>
       <div>
         <Button onClick={handleStartStop} id="start_stop" name="play_pause" />
-        <Button id="reset" name="restart_alt" onClick={reset}/>
+        <Button id="reset" name="restart_alt" onClick={reset} />
       </div>
     </div>
   );
@@ -142,18 +162,9 @@ const App = () => {
   return (
     <div>
       <h1>Clock</h1>
-      <Break 
-        state={state}
-        setState={setState}
-      />
-      <Session 
-        state={state}
-        setState={setState}
-      />
-      <Timer 
-        state={state} 
-        setState={setState} 
-      />
+      <Break state={state} setState={setState} />
+      <Session state={state} setState={setState} />
+      <Timer state={state} setState={setState} />
     </div>
   );
 };
